b18ec28115c745dc51f2eea639875e2b93189c6d
Samantha Callie <Hoomee90@gmail.com>
2023-12-15 14:29:41 -0800

    add test for age calculator age property

--- __tests__/AgeCalculator.test.js
+++ __tests__/AgeCalculator.test.js
@@ -5,10 +5,14 @@ describe(`AgeCalculator`, () => {
   let ageCalculator;
 
   beforeEach(() => {
-    ageCalculator = new AgeCalculator();
+    ageCalculator = new AgeCalculator(18);
   });
 
   test(`should correctly create an Age Calculator object` , () => {
     expect(typeof ageCalculator).toEqual(`object`);
+  });
+
+  test(`should have an age property equal to inputted age argument` , () => {
+    expect(ageCalculator.age).toEqual(18);
   });
 });
