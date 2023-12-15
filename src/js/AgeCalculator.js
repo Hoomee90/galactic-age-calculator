dd56d4e2ff61e15fce902acda99f507ecb2a9d4a
Samantha Callie <Hoomee90@gmail.com>
2023-12-15 15:09:24 -0800

    add get planet mod method

--- src/js/AgeCalculator.js
+++ src/js/AgeCalculator.js
@@ -4,15 +4,20 @@ export default class AgeCalculator {
     this.planetModifiers = new Map([[`earth`, 1]])
   }
 
+  getPlanetMod(planet) {
+    const planetMod = this.planetModifiers.has(planet) ? this.planetModifiers.get(planet) : Array.from(this.planetModifiers.values())[planet];
+    
+    return planetMod;
+  }
+
   newAge(age) {
     this.age = parseInt(age)
   }
 
   getAge(planetInput) {
-    const planet = this.planetModifiers.has(planetInput) ? planetInput : Array.from(this.planetModifiers)[planetInput][0];
-    
-    const calculatedAge = this.age * this.planetModifiers.get(planet);
 
-    return calculatedAge;
+    const planetMod = this.getPlanetMod(planetInput);
+
+    return this.age * planetMod;
   }
 }
