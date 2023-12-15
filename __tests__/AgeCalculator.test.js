6dbd45e11da577464bc2ecb4f729243af62a7f84
Samantha Callie <Hoomee90@gmail.com>
2023-12-15 14:31:43 -0800

    add test for new age method

--- __tests__/AgeCalculator.test.js
+++ __tests__/AgeCalculator.test.js
@@ -12,7 +12,12 @@ describe(`AgeCalculator`, () => {
     expect(typeof ageCalculator).toEqual(`object`);
   });
 
-  test(`should have an age property equal to inputted age argument` , () => {
+  test(`should have an age property equal to inputted age argument`, () => {
     expect(ageCalculator.age).toEqual(18);
+  });
+
+  test(`newAge should replace the age property with something new`, () => {
+    ageCalculator.newAge(413);
+    expect(ageCalculator.age).toEqual(413);
   });
 });
