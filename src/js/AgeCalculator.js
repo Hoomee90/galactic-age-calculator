0f9e783dfff4c9b55ece0f24d9ec7fa66517a9ca
Samantha Callie <Hoomee90@gmail.com>
2023-12-15 14:59:07 -0800

    add get age method

--- src/js/AgeCalculator.js
+++ src/js/AgeCalculator.js
@@ -1,9 +1,18 @@
 export default class AgeCalculator {
   constructor(age) {
     this.age = parseInt(age);
+    this.planetModifiers = new Map([[`earth`, 1]])
   }
 
   newAge(age) {
     this.age = parseInt(age)
+  }
+
+  getAge(planetInput) {
+    const planet = this.planetModifiers.has(planetInput) ? planetInput : Array.from(this.planetModifiers)[planetInput][0];
+    
+    const calculatedAge = this.age * this.planetModifiers.get(planet);
+
+    return calculatedAge;
   }
 }
