d57789822780fbd6f788670b7493fcd0409a183c
Samantha Callie <Hoomee90@gmail.com>
2023-12-15 14:35:18 -0800

    add test for get age method

--- __tests__/AgeCalculator.test.js
+++ __tests__/AgeCalculator.test.js
@@ -1,12 +1,12 @@
 import AgeCalculator from "../src/js/AgeCalculator"
 
-describe(`AgeCalculator`, () => {
-
-  let ageCalculator;
+let ageCalculator;
 
   beforeEach(() => {
     ageCalculator = new AgeCalculator(18);
   });
+
+describe(`AgeCalculator`, () => {
 
   test(`should correctly create an Age Calculator object` , () => {
     expect(typeof ageCalculator).toEqual(`object`);
@@ -19,5 +19,13 @@ describe(`AgeCalculator`, () => {
   test(`newAge should replace the age property with something new`, () => {
     ageCalculator.newAge(413);
     expect(ageCalculator.age).toEqual(413);
+  });
+});
+
+describe(`getAge`, () => {
+
+  test(`should return age property value when given either 'earth' or 0` , () => {
+    expect(ageCalculator.getAge(`earth`)).toEqual(18);
+    expect(ageCalculator.getAge(0)).toEqual(18);
   });
 });
