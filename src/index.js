3213b806d0aee241e5769e1a96a2c401e1978674
Samantha Callie <Hoomee90@gmail.com>
2023-12-20 14:50:47 -0800

    fix typo

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import AgeCalculator from './js/AgeCalculator';"./js/AgeCalculator"
+import AgeCalculator from "./js/AgeCalculator";
